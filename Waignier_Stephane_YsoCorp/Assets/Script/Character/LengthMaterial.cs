b5525f0debfb28a657f155ff5a7f4b7c347cad7b
Waignier Stéphane <stephwgr59@gmail.com>
2022-10-26 00:04:47 +0200

    commit

---
+++ Waignier_Stephane_YsoCorp/Assets/Script/Character/LengthMaterial.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LengthMaterial : MonoBehaviour
+{
+    public Material[] _materialRandom;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
